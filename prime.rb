96eb74f916e6e4e99004ab1eb1aa15dffe2d9a4d
Jonathan Norman <jnorman2123@gmail.com>
2019-10-09 18:14:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,9 @@ def prime?(integer)
   i = 3
   if i == 1 || i = 2
     return true
-  else
-    while i < integer
-      if integer < 0
-        return false
+  elsif integer < 0
+      return false
+  else while i < integer
       elsif integer % i == 0
         return false
       else
