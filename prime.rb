b08aba869583aaf59abde0b7f6969294a6e1798c
Jonathan Norman <jnorman2123@gmail.com>
2019-10-09 18:09:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+
+def prime?(integer)
+  i = 3
+  if i == 1 || i = 2
+    return true
+  else
+    while i < integer
+      if integer % i == 0
+        false
+      elsif integer < 0
+        false
+      else
+        i += 1
+      end
+    end
+  end
+  true
+end
